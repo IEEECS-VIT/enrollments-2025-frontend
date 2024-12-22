a6781eb9fe03813098773a4056b586ff1f03b313
Medhansh Jain <medhanshjain2010@gmail.com>
2024-12-22 16:08:23 +0530

    Revert "All files added"
    This reverts commit 24bbd7e9d8a26f8af1554d301a38a2a870aeef5c.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        custom: ['retro', 'sans-serif'],
-        'press-start': ['"Press Start 2P"', 'cursive'],
-      },
-      letterSpacing:{
-        'extra-wide': '1.5em'
-      },
-      colors:{
-          brownish:'#F8B95A',
-      },
-      boxShadow: {
-        'red-glow': '5px 5px 0px #FF4500', 
-      },
-      textShadow: {
-        'glow': '0 0 10px #FF4500, 0 0 20px #FF4500, 0 0 30px #FF4500',
-      },
-      
-    },
+    extend: {},
   },
-  plugins: [
-    require('tailwindcss-textshadow'),
-  ],
+  plugins: [],
 };
-
