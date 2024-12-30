6186e3569ec324aedda408e82e17014e5f27b81d
Aniruddha Neema <aniruddhaneema@gmail.com>
2024-12-30 16:29:53 +0530

    feat: added profile section

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import Treecloud from './Treecloud';
+import ProfileInfo from './ProfileInfo';
+
+export default function Profile(){
+    return(
+            <div className="relative min-h-screen flex items-center justify-center">
+              <div className="absolute w-full z-20 pointer-events-none">
+                <Treecloud />
+              </div>
+        
+              <div className="absolute w-full z-10 pointer-events-auto">
+                <ProfileInfo></ProfileInfo>
+              </div>
+            </div>
+    )
+}
