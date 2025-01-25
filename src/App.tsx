700e09d047744616927a00fc81f014f9ab55ec11
Aniruddha Neema <aniruddhaneema@gmail.com>
2025-01-25 17:21:53 +0530

    feat: added dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Domainselection from './components/Domainselection';
-import Designdomain from './components/Designdomain';
-import Technicaldomain from './components/Technicaldomain';
-import Managementdomain from './components/Managementdomain';
-import Bg from './components/bg';
-import Navbar from './components/Navbar';
-import Landing from './components/Landing';
-import Start from './components/Start';
-import Profile from './components/Profile';
-import ProtectedRoute from './components/ProtectedRoute';
-import Quiz1 from './components/Quiz1';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Domainselection from "./components/Domainselection";
+import Designdomain from "./components/Designdomain";
+import Technicaldomain from "./components/Technicaldomain";
+import Managementdomain from "./components/Managementdomain";
+import Bg from "./components/bg";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import Start from "./components/Start";
+import Profile from "./components/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Quiz1 from "./components/Quiz1";
 import Faq from "./components/Faq";
-import UsernameForm from './components/Username';
-
+import UsernameSection from "./components/UsernameSection";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   return (
@@ -25,7 +25,6 @@ const App = () => {
             <Route index element={<Start />} />
             <Route path="/landing" element={<Landing />} />
 
-
             <Route
               path="/"
               element={
@@ -33,16 +32,16 @@ const App = () => {
                   <div />
                 </ProtectedRoute>
               }
-              >
+            >
               <Route path="domain" element={<Domainselection />} />
               <Route path="design" element={<Designdomain />} />
               <Route path="technical" element={<Technicaldomain />} />
               <Route path="management" element={<Managementdomain />} />
               <Route path="profile" element={<Profile />} />
-              <Route path="username" element={<UsernameForm />} />
-              <Route path="username" element={<UsernameForm />} />
+              <Route path="username" element={<UsernameSection />} />
               <Route path="quiz" element={<Quiz1 />} />
-              <Route path="faq" element={<Faq />} />
+              <Route path="faqs" element={<Faq />} />
+              <Route path="dashboard" element={<Dashboard />} />
             </Route>
           </Routes>
         </div>
