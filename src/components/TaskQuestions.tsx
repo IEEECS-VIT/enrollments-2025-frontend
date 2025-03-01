0bba77f241473d2980235960a5603eec8fb7d0df
ShubhamPrasad3012 <shubhamprasadclass10@gmail.com>
2025-03-02 03:24:10 +0530

    updated tasks ui

--- src/components/TaskQuestions.tsx
+++ src/components/TaskQuestions.tsx
@@ -5,8 +5,15 @@ import WebTask from "./WebTask";
 import AppTask from "./AppTask";
 import AITask from "./AITask";
 import BackendTask from "./BackendTask";
+import CCTask from "./CCTask";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { GoArrowLeft } from "react-icons/go";
+
 
 export default function TaskQuestions() {
+  const [activeTask, setActiveTask] = useState("web");
+  const navigate = useNavigate();
   useEffect(() => {
     // Initialize highlight.js
     hljs.highlightAll();
@@ -20,15 +27,15 @@ export default function TaskQuestions() {
       >
         <div className="flex flex-col items-center sm:flex-row justify-between">
           <div className="text-center gap-x-8 flex">
-            <div className="h-10 w-10 items-center justify-center flex border-2 border-white rounded-md">
-              {/* Icon can be placed here */}
+            <div className="h-10 w-10 items-center cursor-pointer justify-center flex rounded-md"onClick={() => navigate("/dashboard")}>
+            <GoArrowLeft size={40}/>
             </div>
             <p className="text-5xl font-bold font-playmegames tracking-widest text-[#F8B95A]">
               WEB
             </p>
           </div>
 
-          <div className="flex flex-wrap gap-8 justify-center">
+          {/* <div className="flex flex-wrap gap-8 justify-center">
             {["EASY", "MEDIUM", "HARD"].map((level) => (
               <button
                 key={level}
@@ -37,16 +44,43 @@ export default function TaskQuestions() {
                 {level}
               </button>
             ))}
+          </div> */}
+          <div className="flex gap-8">
+          <button
+          className={`ring-2 ring-[#F8B95A] font-playmegames rounded-md shadow-red-glow text-white mt-2 sm:mt-0 h-12 sm:h-12 text-xl sm:text-2xl px-2 lg:px-6 py-2 border border-[#F8B95A] bg-opacity-50 flex items-center justify-center hover:scale-105 transition-transform duration-300 ${
+            activeTask === "web" ? "bg-[#F8B95A]" : "bg-transparent"
+          }`}
+          onClick={() => setActiveTask("web")}
+        >
+          FRONTEND
+        </button>
+        <button
+          className={`ring-2 ring-[#F8B95A] font-playmegames rounded-md shadow-red-glow text-white mt-2 sm:mt-0 h-12 sm:h-12 text-xl sm:text-2xl px-2 lg:px-6 py-2 border border-[#F8B95A] bg-opacity-50 flex items-center justify-center hover:scale-105 transition-transform duration-300 ${
+            activeTask === "backend" ? "bg-[#F8B95A]" : "bg-transparent"
+          }`}
+          onClick={() => setActiveTask("backend")}
+        >
+          BACKEND
+        </button>
           </div>
+          <button
+                className="ring-2 ring-[#F8B95A] font-playmegames rounded-md shadow-red-glow text-white mt-2 sm:mt-0 h-12 sm:h-12 text-xl sm:text-2xl px-2 lg:px-6 py-2 border border-[#F8B95A] bg-[#F8B95A] bg-opacity-50 flex items-center justify-center hover:scale-105 transition-transform duration-300"
+              >
+                SUBMIT LINKS
+              </button>
         </div>
 
-        <div className="border border-white rounded-3xl mt-8 sm:mt-8 w-full p-4 mb-2 flex flex-col justify-center items-center">
+        <div className="border border-white rounded-3xl mt-8 sm:mt-8 w-full p-4 mb-2 flex flex-col justify-center items-center ">
+          
           {/* <WebTask></WebTask> */}
-          {/* <AppTask></AppTask> */}
+          {/* <AppTask></AppTask> */} 
           {/* <AITask></AITask> */}
           {/* <BackendTask></BackendTask> */}
+          {/* {<CCTask></CCTask>} */}
+          {activeTask === "web" ? <WebTask /> : <BackendTask />}
         </div>
       </div>
     </div>
   );
 }
+
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -11,7 +11,7 @@ export default function Task() {
 
         <div className="absolute w-full z-10 pointer-events-auto ">
           <div className="text-white min-h-screen flex flex-col items-center justify-center font-press-start">
-            <div className="border-2 border-white  backdrop-blur-[4.5px] rounded-3xl w-[80%] lg:w-[90vw] sm:h-[90vh] h-[70vh] flex flex-col items-center p-4 md:p-8">
+            <div className="border-2 border-white  backdrop-blur-[4.5px] rounded-3xl w-[80%] lg:w-[90vw] sm:h-[95vh] h-[70vh] flex flex-col items-center p-4 md:p-8">
               {/* <h2 className="text-5xl my-4 md:m-1 font-playmegames">EVENTS</h2> */}
               <TaskQuestions />
             </div>
