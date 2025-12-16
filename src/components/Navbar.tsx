fdcb71b2dd17376991f10142ed3fec19f32cb7c8
anubhav-kayal <kayalanubhav14@gmail.com>
2025-12-16 12:22:24 +0530

    Discord is added back.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
     { name: "HOME", path: "/" },
     { name: "DOMAINS", path: "/domain" },
     // { name: "DASHBOARD", path: "/dashboard" },
-    //{ name: "DISCORD", path: "https://discord.com/invite/j2Pt6A4YNK" },
+    { name: "DISCORD", path: "https://discord.gg/CwSFPtKh" },
     { name: "FAQS", path: "/faqs" },
   ];
 
@@ -38,7 +38,7 @@ export default function Navbar() {
     { name: "HOME", path: "/" },
     { name: "DOMAINS", path: "/domain" },
     // { name: "DASHBOARD", path: "/dashboard" },
-    //{ name: "DISCORD", path: "https://discord.com/invite/j2Pt6A4YNK" },
+    { name: "DISCORD", path: "https://discord.gg/CwSFPtKh" },
     { name: "PROFILE", path: "/profile" },
     { name: "FAQS", path: "/faqs" },
   ];
