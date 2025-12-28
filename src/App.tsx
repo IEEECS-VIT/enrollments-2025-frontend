76acf32cfaaa7b8b20e09f97f0ce5fcc83f62dfd
anubhav-kayal <kayalanubhav14@gmail.com>
2025-12-28 13:04:03 +0530

    Fixed the App.tsx -- disabled dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -54,8 +54,8 @@ const PageTracker: React.FC = () => {
 const AppContent = () => {
   const location = useLocation();
   useEffect(() => {
-    //disableDevTools();
-    //disableRightClick();
+    disableDevTools();
+    disableRightClick();
   }, []);
 
   return (
